e444d8d54388bd466e460e1be78cf890731784a5
mihir <mihirpamnani31@gmail.com>
2024-02-24 15:49:24 -0600

    fixed logic

--- ola.py
+++ ola.py
@@ -161,17 +161,18 @@ class GroupBySumOla(OLA):
             Update the running grouped sums with a dataframe slice.
         """
         # Implement me!
-        self.rows_seen += len(df_slice)
         grouped_df_slice = df_slice.groupby(self.groupby_col)
+        
         for group_key, group_values in grouped_df_slice :
             group_sum = group_values[self.sum_col].sum()
 
             scaled_sum = group_sum * (self.original_df_num_rows / self.rows_seen)
 
             if group_key in self.group_sums:
-                self.group_sums[group_key] += scaled_sum
+                self.group_sums[group_key] += scaled_sum * (len(df_slice)/ self.original_df_num_rows)
             else:
-                self.group_sums[group_key] = scaled_sum
+                self.group_sums[group_key] = scaled_sum * (len(df_slice) / self.original_df_num_rows)
+        self.rows_seen += len(df_slice)
         # Update the plot
         # hint: self.update_widget(*list of groups*, *list of estimated grouped sums of sum_col*)
         group_key_names = list(self.group_sums.keys())
@@ -203,7 +204,7 @@ class GroupByCountOla(OLA):
             Update the running grouped counts with a dataframe slice.
         """
         # Implement me!
-        self.rows_seen += len(df_slice)
+       
         grouped_df_slice = df_slice.groupby(self.groupby_col)
         for group_key, group_values in grouped_df_slice :
             group_count = group_values[self.count_col].count()
@@ -211,13 +212,14 @@ class GroupByCountOla(OLA):
             scaled_count = group_count * (self.original_df_num_rows / self.rows_seen)
 
             if group_key in self.group_counts:
-                self.group_counts[group_key] += scaled_count
+                self.group_counts[group_key] += scaled_count * (len(df_slice)/ self.original_df_num_rows)
             else:
-                self.group_counts[group_key] = scaled_count
+                self.group_counts[group_key] = scaled_count * (len(df_slice)/ self.original_df_num_rows)
 
         for group_key in self.group_counts:
             group_count = self.group_counts[group_key]
 
+        self.rows_seen += len(df_slice)
         # Update the plot
         # hint: self.update_widget(*list of groups*, *list of estimated group counts of count_col*)
         group_key_names = list(self.group_counts.keys())
